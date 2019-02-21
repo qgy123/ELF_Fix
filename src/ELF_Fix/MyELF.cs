65b3468129c51522b62a04bb552935592068e95c
Yueluo <qgy123@users.noreply.github.com>
2019-02-21 11:45:28 +0800

    Initial commit

---
+++ src/ELF_Fix/MyELF.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ELFSharp.ELF;
+using ELFSharp.ELF.Sections;
+using ELFSharp.ELF.Segments;
+using ELFSharp.Utilities;
+using Syroot.BinaryData;
+
+namespace ELF_Fix
+{
+    public class MyELF<T>/* : ELF<T> where T : struct*/
+    {
+        public string Path { get; }
+        public string FixPath { get; }
+
+        public Class Class { get; private set; }
+        public Endianess Endianess { get; private set; }
+        public FileType Type { get; private set; }
+        public Machine Machine { get; private set; }
+        public T EntryPoint { get; private set; }
+        public T MachineFlags { get; private set; }
+
+        private readonly FileStream stream;
+        private readonly FileStream streamNeedFix;
+        private readonly BinaryStream binaryWriter;
+
+        private Int64 segmentHeaderOffset;
+        private Int64 sectionHeaderOffset;
+        private UInt16 elfHeaderSize;
+        private UInt16 segmentHeaderEntrySize;
+        private UInt16 segmentHeaderEntryCount;
+        private UInt16 sectionHeaderEntrySize;
+        private UInt16 sectionHeaderEntryCount;
+        private UInt16 stringTableIndex;
+        private Func<SimpleEndianessAwareReader> readerSource;
+        private Func<SimpleEndianessAwareReader> localReaderSource;
+
+        public MyELF(string path)
+        {
+            Path = path;
+            FixPath = path + "_fixed";
+            stream = GetNewStream();
+            if (File.Exists(FixPath))
+            {
+                File.Delete(FixPath);
+            }
+            File.Copy(Path, FixPath);
+
+            streamNeedFix = GetRwStream();
+            binaryWriter = new BinaryStream(streamNeedFix);
+            
+            //ReadAndFixHeader();
+            //ReadStringTable();
+            //ReadSections();
+            //ReadSegmentHeaders();
+        }
+
+        public void PrintHeaderInfo()
+        {
+            Console.WriteLine("Elf Header:");
+            Console.WriteLine($"->Class: {Class}");
+            Console.WriteLine($"->Endianess: {Endianess}");
+            Console.WriteLine($"->FileType: {Type}");
+            Console.WriteLine($"->Machine: {Machine}");
+            Console.WriteLine($"->EntryPoint: {EntryPoint}");
+            Console.WriteLine($"->MachineFlags: {MachineFlags}");
+            Console.WriteLine();
+            Console.WriteLine($"->Program Header Size(e_phoff): {segmentHeaderOffset}");
+            Console.WriteLine($"->Section Header Offset(e_shoff): {sectionHeaderOffset}");
+            Console.WriteLine($"->Elf Header Size(e_ehsize): {elfHeaderSize}");
+            Console.WriteLine($"->Program Header Entry Size(e_phentsize): {segmentHeaderEntrySize}");
+            Console.WriteLine($"->Program Header Entries(e_phnum): {segmentHeaderEntryCount}");
+            Console.WriteLine($"->Section Header Entry Size(e_shentsize): {sectionHeaderEntrySize}");
+            Console.WriteLine($"->Section Header Entries(e_shnum): {sectionHeaderEntryCount}");
+            Console.WriteLine($"->String Table Index(e_shtrndx): {stringTableIndex}");
+        }
+
+        public void ReadAndFixHeader()
+        {
+            ReadAndFixIdentificator();
+            readerSource = () => new SimpleEndianessAwareReader(GetNewStream(), Endianess);
+            localReaderSource = () => new SimpleEndianessAwareReader(stream, Endianess, true);
+            ReadFields();
+        }
+
+        private void ReadFields()
+        {
+            using (var reader = localReaderSource())
+            {
+                Type = (FileType)reader.ReadUInt16();
+                Machine = (Machine)reader.ReadUInt16();
+                var version = reader.ReadUInt32();
+                if (version != 1)
+                {
+                    Console.WriteLine($"Given ELF file is of unknown version {version}.");
+                    Console.WriteLine($"Fix to 1.");
+                    binaryWriter.Position = reader.BaseStream.Position - 1;
+                    binaryWriter.WriteByte(1);
+                }
+                EntryPoint = (Class == Class.Bit32 ? reader.ReadUInt32() : reader.ReadUInt64()).To<T>();
+                // TODO: assertions for (u)longs
+                segmentHeaderOffset = Class == Class.Bit32 ? reader.ReadUInt32() : reader.ReadInt64();
+                sectionHeaderOffset = Class == Class.Bit32 ? reader.ReadUInt32() : reader.ReadInt64();
+                MachineFlags = reader.ReadUInt32().To<T>(); // TODO: always 32bit?
+                elfHeaderSize = reader.ReadUInt16(); // elf header size
+                segmentHeaderEntrySize = reader.ReadUInt16();
+                segmentHeaderEntryCount = reader.ReadUInt16();
+                sectionHeaderEntrySize = reader.ReadUInt16();
+                sectionHeaderEntryCount = reader.ReadUInt16();
+                stringTableIndex = reader.ReadUInt16();
+            }
+        }
+
+
+        private void ReadAndFixIdentificator()
+        {
+            var reader = new BinaryReader(stream);
+            reader.ReadBytes(4); // ELF magic
+            var classByte = reader.ReadByte();
+            switch (classByte)
+            {
+                case 1:
+                    Class = Class.Bit32;
+                    break;
+                case 2:
+                    Class = Class.Bit64;
+                    break;
+                default:
+                    throw new ArgumentException($"Given ELF file is of unknown class {classByte}.");
+            }
+            var endianessByte = reader.ReadByte();
+            switch (endianessByte)
+            {
+                case 1:
+                    Endianess = Endianess.LittleEndian;
+                    binaryWriter.ByteConverter = new ByteConverterLittle();
+                    break;
+                case 2:
+                    Endianess = Endianess.BigEndian;
+                    binaryWriter.ByteConverter = new ByteConverterBig();
+                    break;
+                default:
+                    Console.WriteLine($"Given ELF file uses unknown endianess {endianessByte}.");
+                    Console.WriteLine($"Fix to LittleEndian.");
+                    Endianess = Endianess.LittleEndian;
+                    binaryWriter.ByteConverter = new ByteConverterLittle();
+                    binaryWriter.Position = reader.BaseStream.Position - 1;
+                    binaryWriter.WriteByte(1);
+                    //streamNeedFix.Position = reader.BaseStream.Position - 1;
+                    //streamNeedFix.WriteByte(1);
+                    break;
+            }
+            reader.ReadBytes(10); // padding bytes of section e_ident
+        }
+
+        private FileStream GetNewStream()
+        {
+            return new FileStream(
+                Path,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read
+            );
+        }
+
+        private FileStream GetRwStream()
+        {
+            return new FileStream(
+                FixPath,
+                FileMode.Create,
+                FileAccess.ReadWrite,
+                FileShare.None
+            );
+        }
+
+        public void Dispose()
+        {
+            stream.Close();
+        }
+    }
+}
+
+namespace ELF_Fix
+{
+    internal static class Utilities
+    {
+        internal static T To<T>(this object source)
+        {
+            return (T)Convert.ChangeType(source, typeof(T));
+        }
+    }
+}
---
+++ src/ELF_Fix/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ELFSharp.ELF;
+using Console = System.Console;
+
+namespace ELF_Fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInfo();
+            var path = Console.ReadLine();
+            path = path?.Replace("\"", "");
+            if (File.Exists(path))
+            {
+                var type = ELFReader.CheckELFType(path);
+
+                switch (type)
+                {
+                    case Class.Bit32:
+                        Console.WriteLine("32Bit Elf detected!");
+                        var myElf = new MyELF<Int32>(path);
+                        myElf.ReadAndFixHeader();
+                        myElf.PrintHeaderInfo();
+                        break;
+                    case Class.Bit64:
+                        Console.WriteLine("64Bit Elf is not support currently!");
+                        return;
+                        break;
+                    case Class.NotELF:
+                        Console.WriteLine("Given File is not a valid Elf! Make sure the magic is correct!");
+                        return;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        static void PrintInfo()
+        {
+            Console.WriteLine("\'    _____  _      _____   _____  ___ __  __ _____  ____  \r\n\'   | ____|| |    |  ___| |  ___||_ _|\\ \\/ /| ____||  _ \\ \r\n\'   |  _|  | |    | |_    | |_    | |  \\  / |  _|  | |_) |\r\n\'   | |___ | |___ |  _|   |  _|   | |  /  \\ | |___ |  _ < \r\n\'   |_____||_____||_|     |_|    |___|/_/\\_\\|_____||_| \\_\\\r\n\'                                                         ");
+            Console.WriteLine("Author: Yueluo");
+            Console.WriteLine();
+            Console.WriteLine("Please drag your Elf file here:");
+        }
+    }
+}
